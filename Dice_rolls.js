c29228069e2aeb223832f604f5922c4121537c1e
dtirnanic <dejan.tirnanic85@gmail.com>
2020-02-26 08:31:05 -0600

    Created RollDice method with random class implemented

---
+++ Dice_rolls.js
@@ -0,0 +1,33 @@
+function RollDice(sidedDie){
+
+    var d1 = Math.floor(Math.random() * sidedDie) +1;
+    var d2 = Math.floor(Math.random() * sidedDie) +1;
+    var diceTotal = d1 + d2;
+}
+
+function Round1(){
+
+}
+function Round2(){
+
+}
+function Round3(){
+
+}
+function Round4(){
+
+}
+function Round5(){
+
+}
+function Round6(){
+
+}
+
+let playerOne = RollDice(6);
+
+
+
+
+ 
+
